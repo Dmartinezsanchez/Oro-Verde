611df1ef0ca98a8eceb222e20c0b3c318c3c3498
nisofi20 <nisofi20@gmail.com>
2024-09-01 21:07:27 -0500

    Se ajusta indentacion en archivo de JS

--- static/JS/script.js
+++ static/JS/script.js
@@ -19,22 +19,22 @@ function topFunction() {
 
 document.addEventListener('DOMContentLoaded', function () {
   // Seleccionar todos los elementos con las clases flash-success y flash-error
-  var flashMessages = document.querySelectorAll('.flash-success, .flash-error');
+var flashMessages = document.querySelectorAll('.flash-success, .flash-error');
 
   // Recorrer todos los mensajes flash seleccionados
-  flashMessages.forEach(function (message) {
+flashMessages.forEach(function (message) {
       // Después de 3 segundos (3000 ms), añadir la clase 'fade-out' para iniciar el efecto de desvanecimiento
-      setTimeout(function () {
-          message.classList.add('fade-out');
+    setTimeout(function () {
+        message.classList.add('fade-out');
       }, 3000); // Ajusta el tiempo antes de que empiece a desaparecer
 
       // Después de 4 segundos (4000 ms), eliminar el mensaje completamente del DOM
-      setTimeout(function () {
-          if (message.parentNode) {
-              message.parentNode.removeChild(message);
-          }
+    setTimeout(function () {
+        if (message.parentNode) {
+            message.parentNode.removeChild(message);
+        }
       }, 4000); // Ajusta el tiempo total del efecto
-  });
+});
 });
 
 
