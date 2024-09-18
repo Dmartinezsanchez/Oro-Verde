a7036c808f950b904e99da330d0a998e2575b0ae
EsGaRa <egr9524@gmail.com>
2024-09-17 20:36:38 -0500

    Configurando entorno

--- env/Scripts/activate.bat
+++ env/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\Intel\OneDrive\Desktop\Tienda naturista\env
+set VIRTUAL_ENV=C:\Users\estef\OneDrive\Documentos\SENA ADSO\Oro-Verde\env
 
 if not defined PROMPT set PROMPT=$P$G
 
